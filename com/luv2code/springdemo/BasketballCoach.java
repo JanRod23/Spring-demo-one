0b220a3a6627ccd988492bc98bf77150b6d60ebd
Jansen Rodriguez <JansenR125@gmail.com>
2021-01-07 14:57:36 -0500

    Added basketball coach

---
+++ com/luv2code/springdemo/BasketballCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class BasketballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Make 105 three pointers in a row";
+	}
+	
+}
